754c3445409da5b25381c926fcde7da2f790e758
lamekammi <kameronnb@gmail.com>
2022-08-02 16:38:02 -0700

    MERN-Stack Infrastructure

---
+++ src/components/SignUpForm.jsx/SignUpForm.jsx
@@ -0,0 +1,60 @@
+import { Component, useState } from "react";
+import { signUp } from '../../utilities/users-service';
+
+export default class SignUpForm extends Component {
+    state= {
+        name: '',
+        email: '',
+        password: '',
+        confirm: '',
+        error: ''
+    };
+
+    handleChange = (evt) => {
+      this.setState({
+        [evt.target.name]: evt.target.value,
+         error: ''
+      });
+    };
+
+    handleSubmit = async (evt) => {
+      evt.preventDefault();
+      try {
+        // We don't want to send the 'error' or 'confirm' property
+        // so lets make a copy of the state object, then delete them
+        const formData = {...this.state};
+        delete formData.error;
+          // The promise returned by the signUp service method 
+          // will resolve to the user object included in the
+          // payload of the JSON Web Token (JWT)
+        delete formData.confirm;
+        const user = await signUp(formData);
+        this.props.setUser(user)
+      } catch {
+        // an error occurred
+        this.setState({ error: 'Sign up failed - Try Again' });
+      }
+    }
+    
+    render() {
+        const disable = this.state.password !== this.state.confirm;
+        return (
+          <div>
+            <div className="form-container">
+              <form autoComplete="off" onSubmit={this.handleSubmit}>
+                <label>Name</label>
+                <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+                <label>Email</label>
+                <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+                <label>Password</label>
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+                <label>Confirm</label>
+                <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+                <button type="submit" disabled={disable}>SIGN UP</button>
+              </form>
+            </div>
+            <p className="error-message">&nbsp;{this.state.error}</p>
+          </div>
+        );
+    }
+}      
---
+++ src/utilities/users-service.js
@@ -0,0 +1,51 @@
+// Import all named exports attached to a usersAPI object
+// This syntax can be helpful documenting where the methods come from 
+import * as usersAPI from './users-api';
+
+
+export async function signUp(userData) {
+    // Delegate the network request code to the users-api.js API module
+    // which will ultimately return a JSON Web Token (JWT)
+    const token = await usersAPI.signUp(userData);
+    localStorage.setItem('token', token);
+    return getUser();
+}
+
+export async function login(userData) {
+    const token = await usersAPI.login(userData);
+    localStorage.setItem('token', token);
+    return getUser();
+}
+
+export function logOut() {
+    localStorage.removeItem('token');
+}
+
+export function getToken() {
+    // getItem returns null if theres no string
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+    // obtain the payload of the token
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    // a JWT's exp is expressed in seconds, not milliseconds so convert
+    if (payload.exp < Date.now() / 1000) {
+        //token has expired - remove it from localStorage
+        localStorage.removeItem('token');
+        return null;
+    }
+    return token;
+}
+
+export function checkToken() {
+     // Just so that you don't forget how to use .then
+  return usersAPI.checkToken()
+  // checkToken returns a string, but let's 
+  // make it a Date object for more flexibility
+  .then(dateStr => new Date(dateStr));
+}
+
+export function getUser() {
+    const token = getToken();
+    //if there's a token, return the user in the payload, otherwise return null
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+}
